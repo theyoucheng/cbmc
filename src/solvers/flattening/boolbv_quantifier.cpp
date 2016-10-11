9da997abf365479831f9d82b3521348569d69714
theyoucheng <youcheng.sun@cs.ox.ac.uk>
2016-10-11 11:50:58 +0100

    merged the quantifier branch

--- src/solvers/flattening/boolbv_quantifier.cpp
+++ src/solvers/flattening/boolbv_quantifier.cpp
@@ -12,260 +12,6 @@ Author: Daniel Kroening, kroening@kroening.com
 #include <util/replace_expr.h>
 
 #include "boolbv.h"
-#include <util/arith_tools.h>
-
-/*******************************************************************\
-  The implementation in this file aims to convert a quantifier 
-  expression into literals. Note that if a quantifier forall/exists
-  is used at the scope of an assertion, it is handled in the
-  procedure of goto-symex and is invisible here.
-  At this stage, 
-
-  1) the quantifier has been simplified in within the 
-     goto-symex stage;
-
-  2) we further assume that the quantifier is applied upon an array.
-
-  E.g.,
-     __CPROVER_assume(__CPROVER_forall{int i; i>=0&&i<=5 ==>
-                      a[i]>0&&a[i]<=100});
-     or
-     __CPROVER_assume(__CPROVER_exists{int i; i>=0&&i<=5 ==>
-                      a[i]>0&&a[i]<=100});
-     or
-     if(__CPROVER_exists{int i; i>=0&&i<=5 ==> a[i]>0&&a[i]<=100});
-\*******************************************************************/
-
-/*******************************************************************\
-
-Function: is_symbol
-
-  Inputs:
-
- Outputs:
-
- Purpose: A method to detect symbol or typecast symbol
-
-\*******************************************************************/
-
-bool is_symbol(const exprt& expr)
-{
-  if(expr.id()==ID_symbol) return true;
-  if(expr.operands().size()==1)
-    if(expr.id()==ID_typecast && expr.op0().id()==ID_symbol)
-      return true;
-  return false;
-}
-
-
-/*******************************************************************\
-
-Function: is_constant
-
-  Inputs:
-
- Outputs:
-
- Purpose: A method to detect symbol or typecast constant
-
-\*******************************************************************/
-
-bool is_constant(const exprt& expr)
-{
-  if(expr.id()==ID_constant) return true;
-  if(expr.operands().size()==1)
-    if(expr.id()==ID_typecast && expr.op0().id()==ID_constant)
-      return true;
-  return false;
-}
-
-/*******************************************************************\
-
-Function: expr_eq
-
-  Inputs:
-
- Outputs:
-
- Purpose:
-
-\*******************************************************************/
-
-bool expr_eq(const exprt &e1, const exprt &e2)
-{
-  exprt ee1=e1, ee2=e2;
-  if(e1.id()==ID_typecast) ee1=e1.op0();
-  if(e2.id()==ID_typecast) ee2=e2.op0();
-  return ee1==ee2;
-}
-
-/*******************************************************************\
-
-Function: get_index_expr
-
-  Inputs:
-
- Outputs:
-
- Purpose: This method fetches the expr that represents the 
-          array index.
-
-\*******************************************************************/
-
-exprt get_index_expr(const exprt &forall_rewritten)
-{
-  exprt res;
-  res.make_false();
-  for(auto &x : forall_rewritten.operands())
-  {
-    /**
-     * Be careful that such a primitive way to fetch index expr
-     * is sensitive to the form of the "simplified quantifier"
-     **/
-    if(x.id()==ID_ge)
-      if(is_symbol(x.op0()) and is_constant(x.op1()))
-      {
-        if(x.op0().id()==ID_symbol)
-          return x.op0();
-        else
-          return x.op0().op0();
-      }
-  }
-  return res;
-}
-
-/*******************************************************************\
-
-Function: get_min_index
-
-  Inputs:
-
- Outputs:
-
- Purpose: To obtain the min index for the array element in the scope
-          of the specified forall/exists operator.
-
-\*******************************************************************/
-
-exprt get_min_index(const exprt &index_expr, const exprt &forall_rewritten)
-{
-  exprt res;
-  res.make_false();
-  for(auto &x : forall_rewritten.operands())
-  {
-    if(x.id()!=ID_not) continue;
-    exprt y=x.op0();
-    if(y.id()!=ID_ge) continue;
-    if(expr_eq(y.op0(), index_expr) && is_constant(y.op1()))
-    {
-      res=y.op1();
-      if(res.id()==ID_typecast) res=res.op0();
-      return res;
-    }
-  }
-  return res;
-}
-
-/*******************************************************************\
-
-Function: get_max_index
-
-  Inputs:
-
- Outputs:
-
- Purpose: To obtain the max index for the array element in the scope
-          of the specified forall/exists operator.
-
-\*******************************************************************/
-
-exprt get_max_index(const exprt &index_expr, const exprt &forall_rewritten)
-{
-  exprt res;
-  res.make_false();
-  for(auto &x : forall_rewritten.operands())
-  {
-    if(x.id()!=ID_ge) continue;
-    if(expr_eq(x.op0(), index_expr) && is_constant(x.op1()))
-    {
-      exprt over_expr=x.op1();
-      if(over_expr.id()==ID_typecast) over_expr=over_expr.op0();
-      mp_integer over_i;
-      to_integer(over_expr, over_i);
-      /**
-       * Due to the ''do_simplify'' called along with goto-symex,
-       * the ''over_i'' value we obtain here is not the exact
-       * maximum index as specified in the original code.
-       **/
-      over_i-=1;
-      res=from_integer(over_i, x.op1().type());
-      return res;
-    }
-  }
-  return res;
-}
-
-/*******************************************************************\
-
-Function: get_quantifier_body
-
-  Inputs:
-
- Outputs:
-
- Purpose: To obtain the body constraint specified within
-          of the given forall/exists operator.
-
-\*******************************************************************/
-
-exprt get_quantifier_body(const exprt &re_quantifier)
-{
-  exprt res;
-  res.make_false();
-  for(auto &x : re_quantifier.operands())
-  {
-    if(x.id()==ID_ge)
-    {
-      if(is_symbol(x.op0()) and is_constant(x.op1()))
-        continue;
-    }
-    if(x.id()==ID_not)
-    {
-      exprt y=x.op0();
-      if(y.id()!=ID_ge) continue;
-      if(is_symbol(x.op0()) and is_constant(x.op1()))
-        continue;
-    }
-    return x;    
-  }
-  return res;
-}
-
-/*******************************************************************\
-
-Function: instantiate(index_expr, index_inst, expr)
-
-  Inputs:
-
- Outputs:
-
- Purpose: Replace all "index_expr"s inside "expr" with "index_inst"
-
-\*******************************************************************/
-void instantiate(const exprt &index_expr,
-                 const mp_integer &index_inst,
-                 exprt &expr)
-{
-  if(expr==index_expr)
-  {
-    expr=from_integer(index_inst, expr.type()); 
-    return;
-  }
-  for(auto &x: expr.operands())
-  {
-    instantiate(index_expr, index_inst, x);
-  }
-}
 
 /*******************************************************************\
 
@@ -281,59 +27,17 @@ Function: boolbvt::convert_quantifier
 
 literalt boolbvt::convert_quantifier(const exprt &expr)
 {
-  assert(expr.operands().size()==2);
-  assert(expr.op0().id()==ID_symbol);
-  /**
-   * We need to rewrite the forall/exists quantifier into
-   * an OR expr with 3 operands: min/max indices of array elements
-   * accessed and the constraint specified on array elements.
-   **/
-  exprt re(expr);
-  exprt tmp(re.op1());
-  re.swap(tmp);
-  exprt index_expr=get_index_expr(re);
-  exprt min_i=get_min_index(index_expr, re);
-  exprt max_i=get_max_index(index_expr, re);
-  exprt body_expr=get_quantifier_body(re);
-  if(index_expr.is_false() ||
-     min_i.is_false() ||
-     max_i.is_false() ||
-     body_expr.is_false())
+  if(expr.operands().size()!=3)
     return SUB::convert_rest(expr);
   
-  quantifier_list.push_back(quantifiert());
-
-  quantifier_list.back().index_expr=index_expr;
-  to_integer(min_i, quantifier_list.back().lb);
-  to_integer(max_i, quantifier_list.back().ub);
-  quantifier_list.back().id=expr.id();
-
   literalt l=prop.new_variable();
+  
+  quantifier_list.push_back(quantifiert());
   quantifier_list.back().l=l;
-
-  // instantiate index-relevant variables in the body if there exists 
-  auto it=quantifier_list.end();
-  it--;
-  if(it->id==ID_exists)
-  {
-    it->expr=body_expr; 
-    return l;
-  }
-  std::vector<exprt> expr_insts;
-  for(mp_integer i=it->lb; i<=it->ub; ++i)
-  {
-    exprt constraint_expr=body_expr;
-    instantiate(it->index_expr, i, constraint_expr);
-    expr_insts.push_back(constraint_expr);
-  }
-  if(it->id==ID_forall)
-  {
-    it->expr=conjunction(expr_insts);
-  }
-
+  quantifier_list.back().expr=expr;
+  
   return l;
 }
-
 
 /*******************************************************************\
 
@@ -352,26 +56,44 @@ void boolbvt::post_process_quantifiers()
   std::set<exprt> instances;
   
   if(quantifier_list.empty()) return;
+  
+  for(quantifier_listt::const_iterator q_it=quantifier_list.begin();
+      q_it!=quantifier_list.end();
+      q_it++)
+    forall_operands(o_it, q_it->expr.op1())
+      instances.insert(*o_it);
 
-  for(auto it=quantifier_list.begin();
-      it!=quantifier_list.end();  
-      ++it)
+  if(instances.empty())
+    throw "post_process_quantifiers: no instances";
+
+  for(quantifier_listt::const_iterator q_it=quantifier_list.begin();
+      q_it!=quantifier_list.end();
+      q_it++)
   {
+    const exprt &expr=q_it->expr;
+    
     bvt inst_bv;
-    for(mp_integer i=it->lb; i<=it->ub; ++i)
+    inst_bv.reserve(instances.size());
+    
+    for(std::set<exprt>::const_iterator it=instances.begin();
+        it!=instances.end();
+        it++)
     {
-      exprt constraint_expr=it->expr;
-      instantiate(it->index_expr, i, constraint_expr);
-      inst_bv.push_back(convert_bool(constraint_expr));
-    }
-    if(it->id==ID_forall)
-    {
-      prop.set_equal(prop.land(inst_bv), it->l);
-    }
-    if(it->id==ID_exists)
-    {
-      prop.set_equal(prop.lor(inst_bv), it->l);
-    }
-  }
+      exprt dest(expr.op2());
 
+      exprt by(*it);
+      if(expr.op0().type()!=by.type())
+        by.make_typecast(expr.op0().type());
+
+      replace_expr(expr.op0(), by, dest);
+      inst_bv.push_back(convert(dest));
+    }
+
+    if(expr.id()==ID_forall)
+      prop.set_equal(prop.land(inst_bv), q_it->l);
+    else if(expr.id()==ID_exists)
+      prop.set_equal(prop.lor(inst_bv), q_it->l);
+    else
+      assert(false);
+  }
 }
