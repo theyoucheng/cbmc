1861e03434b7117a542c2cfa7a55a3e58225d58e
Chris Smowton <chris@smowton.net>
2017-02-23 18:39:14 +0000

    Prototype JSON symtab language
    This will parse a JSON symbol table produced by an external parser.

---
+++ src/json-symtab-language/json_symtab_language.h
@@ -0,0 +1,49 @@
+/*******************************************************************\
+
+Module: JSON symbol table language. Accepts a JSON format symbol
+        table that has been produced out-of-process, e.g. by using
+        a compiler's front-end.
+
+Author: Chris Smowton, chris.smowton@diffblue.com
+
+\*******************************************************************/
+
+#ifndef CPROVER_JSON_SYMTAB_LANGUAGE_JSON_SYMTAB_LANGUAGE_H
+#define CPROVER_JSON_SYMTAB_LANGUAGE_JSON_SYMTAB_LANGUAGE_H
+
+#include <util/language.h>
+#include <util/json.h>
+
+class json_symtab_languaget:public languaget
+{
+ public:
+  bool parse(
+    std::istream &instream,
+    const std::string &path) override;
+
+  bool typecheck(
+    symbol_tablet &symbol_table,
+    const std::string &module) override;
+
+  void show_parse(std::ostream &out) override;
+
+  bool to_expr(
+    const std::string &code,
+    const std::string &module,
+    exprt &expr,
+    const namespacet &ns) override
+  {
+    // Not implemented.
+    return true;
+  }
+
+  languaget *new_language() override
+  {
+    return new json_symtab_languaget();
+  }
+
+ protected:
+  jsont parsed_json_file;
+};
+
+#endif
